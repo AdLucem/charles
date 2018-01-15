691b0797bd9beeadc3ee78a5a869559c92896fe5
AdLucem <ghosalatreyee@gmail.com>
2018-01-16 01:10:23 +0530

    got the app working - cl

--- js/services/config.js
+++ js/services/config.js
@@ -1,10 +1,12 @@
 app.service('ConfigService', function ($http) {
     
-    var config;
-    $http.get("config.json").success(function (data) {
-       config = data;
-    });
+    //var config;
+    //$http.get("config.json").success(function (data) {
+       //config = data;
+    //});
     
-    this.getFBLink = function () { return config.fbLink; };
+    //this.getFBLink = function () { return config.fbLink; };
+
+    this.getFBLink = function () { return "https://charlesdemo.firebaseio.com"; };
     
 });
